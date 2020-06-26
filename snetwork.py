1f3a547424f123a8fb4d791aa07cd2abe7be71f5
James <jameshughes89@gmail.com>
2020-06-25 21:28:42 -0300

    added Watts-Stogatz graphs as one of the random graphs generatable :

--- snetwork.py
+++ snetwork.py
@@ -2,13 +2,16 @@
 Author:     James Hughes
 Date:       June 9, 2020
 
-Version:    0.1
+Version:    0.2
 
 
 Change Log:
     0.1 (June 9, 2020): 
         - Initial version.
 
+    0.2 (June 25, 2020):
+        - Watts-Strogatz Graph added as a type of random network
+        - I am not that confident in it though as the way it is implemented seems to enforce a fixed edge number. In other words, there is no variation in the number of neighbours that we would see in reality 
 
 
 End Change Log
@@ -37,13 +40,15 @@ from ndlib.viz.mpl.DiffusionPrevalence import DiffusionPrevalence
 # Epidemic Setup #
 ##################
 
-def setup_network(alpha, beta, gamma, infected, directory=None, name=None, size=None, edge_p=None):
+def setup_network(alpha, beta, gamma, infected, directory=None, name=None, size=None, edge_p=None, knn=None, rewire_p=None):
 
     # Network topology
     
     # Create Graph  
     if directory == None:
         g = nx.erdos_renyi_graph(size, edge_p)
+    elif edge_p == None:
+        g = watts_strogatz_graph(size, knn, rewire_p)
     else:    
         g = nx.read_adjlist(os.path.join(directory, name), delimiter='\t', nodetype=int)
 
